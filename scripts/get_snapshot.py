92352c100810fe26db5be758003561c543e27bc4
Titus <titus+code@mailbox.org>
2025-01-24 13:31:11 +0100

    Update get_snapshot.py
    This new view will be helpful for one or two nice features on the website.

--- scripts/get_snapshot.py
+++ scripts/get_snapshot.py
@@ -1,5 +1,5 @@
 import requests,json
-tables = ["agencies","stops","routes","trips","trip_stop","calendar","calendar_dates","translations","classes","view_ontd_map","view_ontd_list","view_ontd_details"]
+tables = ["agencies","stops","routes","trips","trip_stop","calendar","calendar_dates","translations","classes","view_ontd_map","view_ontd_list","view_ontd_details","view_ontd_cities"]
 for table in tables:
     response = requests.get(f"https://script.google.com/macros/s/AKfycbwY9zNQFq0urCHsTstWRKxLe0SstWrwyY04tSuDIVb_yRCtTs_HDlRARS-5fqltgEZr/exec?table={table}")
     open(f'./data/latest/{table}.json',"w",encoding="UTF-8").write(response.text)
